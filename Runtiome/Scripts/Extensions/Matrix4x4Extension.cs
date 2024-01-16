a213984b4d691c0b6585a89960e1dc61ee521f13
LCH <ckdgus4002@gmail.com>
2024-01-17 02:59:46 +0900

    Commit.

--- Runtiome/Scripts/Extensions/Matrix4x4Extension.cs
+++ Runtiome/Scripts/Extensions/Matrix4x4Extension.cs
@@ -4,9 +4,6 @@ namespace LCHFramework.Extensions
 {
     public static class Matrix4x4Extensions
     {
-        public static Vector3 GetPosition(this Matrix4x4 matrix)
-            => new Vector3(matrix.m03, matrix.m13, matrix.m23);
-
         public static Matrix4x4 NewPosition(this Matrix4x4 matrix, Vector3 position)
             => Matrix4x4.TRS(position, matrix.rotation, matrix.lossyScale);
     }
