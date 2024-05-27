bd1c98727b47149135e1bf84c882217c357f7162
LCH <ckdgus4002@gmail.com>
2024-05-28 02:48:12 +0900

    Commit.

---
+++ Runtime/Scripts/Utilies/TouchScreenKeyboardUtility.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace LCHFramework.Utilies
+{
+    public static class TouchScreenKeyboardUtility
+    {
+        public static float GetTouchScreenKeyboardHeight()
+        {
+            /// TouchScreenKeyboard.area.height는 Android 에서 0을 반환합니다.
+            /// https://docs.unity3d.com/ScriptReference/TouchScreenKeyboard-area.html
+#if UNITY_ANDROID
+            using var unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            using var rect = new AndroidJavaObject("android.graphics.Rect");
+            var view = unityClass.GetStatic<AndroidJavaObject>("currentActivity").Get<AndroidJavaObject>("mUnityPlayer").Call<AndroidJavaObject>("getView");
+            view.Call("getWindowVisibleDisplayFrame", rect);
+            return Screen.height - rect.Call<int>("height");
+#else
+            return TouchScreenKeyboard.area.height;
+#endif
+        }
+    }
+}
