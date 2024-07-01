fe357cca8b9b3467835e4b3c1495e6bbcafe5ec0
Lee Changhyun <ckdgus4002@gmail.com>
2024-07-02 07:07:38 +0900

    Task.SuppressCancellationThrow 임시 삭제.

--- Runtime/Scripts/Extensions/TaskExtension.cs
+++ Runtime/Scripts/Extensions/TaskExtension.cs
@@ -1,20 +1,19 @@
 using System.Threading;
 using System.Threading.Tasks;
-using LCHFramework.Utilities;
 
 namespace LCHFramework.Extensions
 {
     public static class TaskExtension
     {
-        public static Task<bool> SuppressCancellationThrow(this Task task)
-        {
-            var status = task.Status;
-            if (status == TaskStatus.RanToCompletion) return TaskUtility.CompletedTasks.False;
-            if (status == TaskStatus.Canceled) return TaskUtility.CompletedTasks.True;
-            return task.ToCanceledTask();
-        }
-
-        public static Task<bool> ToCanceledTask(this Task task) => new(() => task.Status == TaskStatus.Canceled, task.GetToken());
+        // public static Task<bool> SuppressCancellationThrow(this Task task)
+        // {
+        //     var status = task.Status;
+        //     if (status == TaskStatus.RanToCompletion) return TaskUtility.CompletedTasks.False;
+        //     if (status == TaskStatus.Canceled) return TaskUtility.CompletedTasks.True;
+        //     return task.ToCanceledTask();
+        // }
+        // 
+        // public static Task<bool> ToCanceledTask(this Task task) => new(() => task.Status == TaskStatus.Canceled, task.GetToken());
         
         public static CancellationToken GetToken(this Task task) => new TaskCanceledException(task).CancellationToken;
     }
