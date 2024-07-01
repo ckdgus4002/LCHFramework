7be46cc5c8bce64f976699bb4fa7931f50382e1b
Lee Changhyun <ckdgus4002@gmail.com>
2024-07-01 20:59:20 +0900

    Commit.

---
+++ Runtime/Scripts/Components/LookAtCameraIK.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LCHFramework.Components
+{
+    [RequireComponent(typeof(Animator))]
+    public class LookAtCameraIK : LCHMonoBehaviour
+    {
+        private Animator Animator => _animator == null ? _animator = GetComponent<Animator>() : _animator;
+        private Animator _animator;
+        
+        
+        
+        private void OnAnimatorIK(int layerIndex)
+        {
+            Animator.SetLookAtPosition(Camera.main.transform.position);
+            Animator.SetLookAtWeight(1);
+        }
+    }
+}
---
+++ Runtime/Scripts/Components/UI/SpriteLooper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace LCHFramework.Components.UI
+{
+    [RequireComponent(typeof(Image))]
+    public class SpriteLooper : LCHMonoBehaviour
+    {
+        [SerializeField] private float speed = 1;
+        [SerializeField] private Sprite[] sprites;
+
+
+        private float _startTime;
+        
+        
+        private Image Image => _image == null ? _image = GetComponent<Image>() : _image;
+        private Image _image;
+
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            _startTime = Time.time;
+        }
+        
+        private void Update()
+        {
+            var frame = sprites.Length * speed;
+            var framePerSecond = 1f / frame;
+            var spriteIndex = (int)((Time.time - _startTime) / framePerSecond) % sprites.Length;
+            Image.sprite = sprites[spriteIndex];
+        }
+    }
+}
