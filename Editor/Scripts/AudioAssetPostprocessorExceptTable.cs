e354fa9db242c49c3b9c658921574710965a2f64
Lee Changhyun <ckdgus4002@gmail.com>
2024-10-21 02:19:57 +0900

    Commit.

--- Editor/Scripts/AudioAssetPostprocessorExceptTable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace LCHFramework.Editor
-{
-    [CreateAssetMenu(fileName = nameof(AudioAssetPostprocessorExceptTable), menuName = nameof(AudioAssetPostprocessorExceptTable) + "/" + "Create")]
-    public class AudioAssetPostprocessorExceptTable : AssetPostprocessorExceptTable<AudioAssetPostprocessorExceptTable>
-    {
-    }
-}
---
+++ Editor/Scripts/AudioPostprocessorExceptTable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace LCHFramework.Editor
+{
+    [CreateAssetMenu(fileName = nameof(AudioPostprocessorExceptTable), menuName = nameof(AudioPostprocessorExceptTable) + "/" + "Create")]
+    public class AudioPostprocessorExceptTable : AssetPostprocessorExceptTable<AudioPostprocessorExceptTable>
+    {
+    }
+}
