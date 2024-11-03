3f564882c70e49d5eb6c46ad07266647b5453f59
Lee Changhyun <ckdgus4002@gmail.com>
2024-11-04 04:19:07 +0900

    Commit.

--- Editor/Scripts/SpriteAtlasAssetPostprocessorExceptTable.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace LCHFramework.Editor
-{
-    [CreateAssetMenu(fileName = nameof(SpriteAtlasAssetPostprocessorExceptTable), menuName = nameof(SpriteAtlasAssetPostprocessorExceptTable) + "/" + "Create")]
-    public class SpriteAtlasAssetPostprocessorExceptTable : AssetPostprocessorExceptTable<SpriteAtlasAssetPostprocessorExceptTable>
-    {
-    }
-}
---
+++ Editor/Scripts/SpriteAtlasPostprocessorExceptTable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace LCHFramework.Editor
+{
+    [CreateAssetMenu(fileName = nameof(SpriteAtlasPostprocessorExceptTable), menuName = nameof(SpriteAtlasPostprocessorExceptTable) + "/" + "Create")]
+    public class SpriteAtlasPostprocessorExceptTable : AssetPostprocessorExceptTable<SpriteAtlasPostprocessorExceptTable>
+    {
+    }
+}
