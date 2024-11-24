6acd4ee649a873a40a6a881873002eb98a576a73
Lee Changhyun <ckdgus4002@gmail.com>
2024-11-25 02:47:16 +0900

    MainThread 를 벗어난 상황 대응.

--- Runtime/Scripts/Utilities/DebugUtility.cs
+++ Runtime/Scripts/Utilities/DebugUtility.cs
@@ -1,43 +1,54 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace LCHFramework.Utilities
 {
+    [DefaultExecutionOrder(int.MinValue)]
+    [InitializeOnLoad]
     public static class Debug
     {
+        static Debug() { isDebugBuild = UnityEngine.Debug.isDebugBuild; }
+        
+        
+        
+        public static bool isDebugBuild { get; }
+        
+        
+        
         public static void Log(object message, Color color, bool onlyDebugBuild = true) => Log($"<color={color}>{message}</color>", onlyDebugBuild);
         
-        public static void Log(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.Log(message); }
+        public static void Log(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.Log(message); }
         
         public static void Log(object message, Object context, Color color, bool onlyDebugBuild = true) => Log($"<color={color}>{message}</color>", context, onlyDebugBuild);
         
-        public static void Log(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.Log(message, context); }
+        public static void Log(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.Log(message, context); }
         
-        public static void LogFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogFormat(format, args); }
+        public static void LogFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogFormat(format, args); }
         
-        public static void LogFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogFormat(context, format, args); }
+        public static void LogFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogFormat(context, format, args); }
         
-        public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogFormat(logType, logOptions, context, format, args); }
+        public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogFormat(logType, logOptions, context, format, args); }
         
-        public static void LogError(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogError(message); }
+        public static void LogError(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogError(message); }
         
-        public static void LogError(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogError(message, context); }
+        public static void LogError(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogError(message, context); }
         
-        public static void LogErrorFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogErrorFormat(format, args); }
+        public static void LogErrorFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogErrorFormat(format, args); }
         
-        public static void LogErrorFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogErrorFormat(context, format, args); }
+        public static void LogErrorFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogErrorFormat(context, format, args); }
         
-        public static void LogException(Exception exception, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogException(exception); }
+        public static void LogException(Exception exception, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogException(exception); }
         
-        public static void LogException(Exception exception, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogException(exception, context); }
+        public static void LogException(Exception exception, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogException(exception, context); }
         
-        public static void LogWarning(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogWarning(message); }
+        public static void LogWarning(object message, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogWarning(message); }
         
-        public static void LogWarning(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogWarning(message, context); }
+        public static void LogWarning(object message, Object context, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogWarning(message, context); }
         
-        public static void LogWarningFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogWarningFormat(format, args); }
+        public static void LogWarningFormat(string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogWarningFormat(format, args); }
         
-        public static void LogWarningFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || UnityEngine.Debug.isDebugBuild) UnityEngine.Debug.LogWarningFormat(context, format, args); }
+        public static void LogWarningFormat(Object context, string format, /*params*/object[] args, bool onlyDebugBuild = true) { if (!onlyDebugBuild || isDebugBuild) UnityEngine.Debug.LogWarningFormat(context, format, args); }
     }
 }
