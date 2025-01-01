bd947b3599ee900762fc85dcf24efb5dc8e0d6c0
Lee Changhyun <ckdgus4002@gmail.com>
2025-01-02 01:13:37 +0900

    Conditional 쪽이 더 빨라서 변경.

--- Runtime/Scripts/Utilities/DebugUtility.cs
+++ Runtime/Scripts/Utilities/DebugUtility.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -6,52 +7,42 @@ namespace LCHFramework.Utilities
 {
     public static class Debug
     {
-        public static bool isDebugBuild
-        {
-            get
-            {
-#if DEBUG
-                return true;
-#else
-                return false;
-#endif
-            }
-        }
+        private const string ConditionString = "DEBUG";
         
         
         
-        public static void Log(object message, Color color) => Log($"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{message}</color>");
+        [Conditional(ConditionString)] public static void Log(object message, Color color) => Log($"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{message}</color>");
         
-        public static void Log(object message) { if (isDebugBuild) UnityEngine.Debug.Log(message); }
+        [Conditional(ConditionString)] public static void Log(object message) => UnityEngine.Debug.Log(message);
         
-        public static void Log(object message, Object context, Color color) => Log($"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{message}</color>", context);
+        [Conditional(ConditionString)] public static void Log(object message, Object context, Color color) => Log($"<color=#{ColorUtility.ToHtmlStringRGBA(color)}>{message}</color>", context);
         
-        public static void Log(object message, Object context) { if (isDebugBuild) UnityEngine.Debug.Log(message, context); }
+        [Conditional(ConditionString)] public static void Log(object message, Object context) => UnityEngine.Debug.Log(message, context);
         
-        public static void LogFormat(string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogFormat(format, args); }
+        [Conditional(ConditionString)] public static void LogFormat(string format, object[] args) => UnityEngine.Debug.LogFormat(format, args);
         
-        public static void LogFormat(Object context, string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogFormat(context, format, args); }
+        [Conditional(ConditionString)] public static void LogFormat(Object context, string format, object[] args) => UnityEngine.Debug.LogFormat(context, format, args);
         
-        public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogFormat(logType, logOptions, context, format, args); }
+        [Conditional(ConditionString)] public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, object[] args) => UnityEngine.Debug.LogFormat(logType, logOptions, context, format, args);
         
-        public static void LogError(object message) { if (isDebugBuild) UnityEngine.Debug.LogError(message); }
+        [Conditional(ConditionString)] public static void LogError(object message) => UnityEngine.Debug.LogError(message);
         
-        public static void LogError(object message, Object context) { if (isDebugBuild) UnityEngine.Debug.LogError(message, context); }
+        [Conditional(ConditionString)] public static void LogError(object message, Object context) => UnityEngine.Debug.LogError(message, context);
         
-        public static void LogErrorFormat(string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogErrorFormat(format, args); }
+        [Conditional(ConditionString)] public static void LogErrorFormat(string format, object[] args) => UnityEngine.Debug.LogErrorFormat(format, args);
         
-        public static void LogErrorFormat(Object context, string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogErrorFormat(context, format, args); }
+        [Conditional(ConditionString)] public static void LogErrorFormat(Object context, string format, object[] args) => UnityEngine.Debug.LogErrorFormat(context, format, args);
         
-        public static void LogException(Exception exception) { if (isDebugBuild) UnityEngine.Debug.LogException(exception); }
+        [Conditional(ConditionString)] public static void LogException(Exception exception) => UnityEngine.Debug.LogException(exception);
         
-        public static void LogException(Exception exception, Object context) { if (isDebugBuild) UnityEngine.Debug.LogException(exception, context); }
+        [Conditional(ConditionString)] public static void LogException(Exception exception, Object context) => UnityEngine.Debug.LogException(exception, context);
         
-        public static void LogWarning(object message) { if (isDebugBuild) UnityEngine.Debug.LogWarning(message); }
+        [Conditional(ConditionString)] public static void LogWarning(object message) => UnityEngine.Debug.LogWarning(message);
         
-        public static void LogWarning(object message, Object context) { if (isDebugBuild) UnityEngine.Debug.LogWarning(message, context); }
+        [Conditional(ConditionString)] public static void LogWarning(object message, Object context) => UnityEngine.Debug.LogWarning(message, context);
         
-        public static void LogWarningFormat(string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogWarningFormat(format, args); }
+        [Conditional(ConditionString)] public static void LogWarningFormat(string format, object[] args) => UnityEngine.Debug.LogWarningFormat(format, args);
         
-        public static void LogWarningFormat(Object context, string format, object[] args) { if (isDebugBuild) UnityEngine.Debug.LogWarningFormat(context, format, args); }
+        [Conditional(ConditionString)] public static void LogWarningFormat(Object context, string format, object[] args) => UnityEngine.Debug.LogWarningFormat(context, format, args);
     }
 }
