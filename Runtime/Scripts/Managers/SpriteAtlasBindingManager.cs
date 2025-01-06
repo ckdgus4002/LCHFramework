b2887d175f06c96c8734b1443a32a161f45aef74
Lee Changhyun <ckdgus4002@gmail.com>
2025-01-06 12:45:18 +0900

    SpriteAtlasBindingManager 추가.

---
+++ Runtime/Scripts/Managers/SpriteAtlasBindingManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace LCHFramework.Managers
+{
+    public class SpriteAtlasBindingManager
+    {
+        private static readonly Dictionary<string, SpriteAtlas> SpriteAtlases = new();
+
+
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void InitializeOnLoadMethod()
+        {
+            SpriteAtlasManager.atlasRequested += AtlasRequested;
+        }
+
+        private static void AtlasRequested(string tag, Action<SpriteAtlas> action)
+        {
+            if (SpriteAtlases.TryGetValue(tag, out var value)) action.Invoke(value);
+        }
+        
+        public static void AddSpriteAtlas(SpriteAtlas spriteAtlas) => SpriteAtlases.Add(spriteAtlas.name, spriteAtlas);
+
+        public static void RemoveSpriteAtlas(SpriteAtlas spriteAtlas) => SpriteAtlases.Remove(spriteAtlas.name);
+    }
+}
