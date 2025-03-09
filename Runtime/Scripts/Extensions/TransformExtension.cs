82de7e698d2954b5e4ef730c86fd00be5e744350
Lee Changhyun <ckdgus4002@gmail.com>
2025-03-09 20:40:51 +0900

    FindInChildren 추가.

--- Runtime/Scripts/Extensions/TransformExtension.cs
+++ Runtime/Scripts/Extensions/TransformExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -42,6 +43,25 @@ namespace LCHFramework.Extensions
                     return -1;
                 
             } while (true);
+        }
+
+        public static Transform FindInChildren(this Transform transform, string n)
+            => transform.FindInChildren(find => find == n);
+        
+        public static Transform FindInChildren(this Transform transform, Func<string, bool> find)
+        {
+            var stack = new Stack<Transform>();
+            var parent = transform;
+            while (true)
+            {
+                for (var i = parent.childCount - 1; 0 <= i; i--) stack.Push(parent.GetChild(i));
+                if (0 < stack.Count)
+                {
+                    parent = stack.Pop();
+                    if (find(parent.name)) return parent;
+                }
+                else return null;
+            }
         }
     }
 }
