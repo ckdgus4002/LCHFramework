e1f4245427494560e54c2be933748286628ebae5
Lee Changhyun <ckdgus4002@gmail.com>
2025-06-09 00:20:56 +0900

    코드 정리.

--- Runtime/Scripts/Managers/MonoSingleton.cs
+++ Runtime/Scripts/Managers/MonoSingleton.cs
@@ -5,17 +5,19 @@ namespace LCHFramework.Managers
 {
     public class MonoSingleton<T> : LCHMonoBehaviour where T : MonoSingleton<T>
     {
-        public static bool InstanceIsNull => Instance == null;
+        public static bool InstanceIsNull => Instance is null;
         
         public static T Instance
         {
-            get => _instance == null ? Instance = FindFirstObjectByType<T>() : _instance;
+            get => _instance ?? (Instance = FindFirstObjectByType<T>());
             private set
             {
                 var prevInstanceOrNull = _instance;
-                _instance = prevInstanceOrNull != null && value != null && !value.DestroyPrevInstance ? prevInstanceOrNull : value;
-                if (prevInstanceOrNull != null && prevInstanceOrNull != value)
-                    Destroy((value == null || value.DestroyPrevInstance ? prevInstanceOrNull : value).DestroyTarget);
+                _instance = prevInstanceOrNull is not null && value is not null && !value.DestroyPrevInstance
+                    ? prevInstanceOrNull
+                    : value;
+                if (prevInstanceOrNull is not null && prevInstanceOrNull != value)
+                    Destroy((value is null || value.DestroyPrevInstance ? prevInstanceOrNull : value).DestroyTarget);
             }
         }
         private static T _instance;
