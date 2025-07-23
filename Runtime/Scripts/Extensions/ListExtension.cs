1bebd35614a1784929a6a0a18b79f5632b7d281a
Lee Changhyun <ckdgus4002@gmail.com>
2025-07-23 20:43:20 +0900

    이름 변경.

--- Runtime/Scripts/Extensions/ListExtension.cs
+++ Runtime/Scripts/Extensions/ListExtension.cs
@@ -4,7 +4,7 @@ namespace LCHFramework.Extensions
 {
     public static class ListExtension
     {
-        public static T AddAndReturn<T>(this List<T> list, T item)
+        public static T AddAndReturnItem<T>(this List<T> list, T item)
         {
             list.Add(item);
             return item;
