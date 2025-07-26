00271c2d57735c39e04b981def21f9edda76ae68
Lee Changhyun <ckdgus4002@gmail.com>
2025-07-26 20:53:56 +0900

    v1.1.2

---
+++ Runtime/Scripts/Components/MonoSingleton.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LCHFramework.Extensions;
+using LCHFramework.Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LCHFramework.Components
+{
+    [DefaultExecutionOrder(EventSystemExecutionOrder - 1)]
+    public class MonoSingleton : AbstractMonoSingleton
+    {
+        private const int EventSystemExecutionOrder = -1000;
+        public const bool DestroyNotMonoSingletonType = true;
+        
+        
+        
+        private static readonly Dictionary<Type, MonoSingleton> instances = new();
+        
+        
+        
+        [SerializeField] private Component singletonType;
+        [SerializeField] private bool isDontDestroyOnLoad;
+        [SerializeField] private bool isDestroyPrevInstance = true;
+        
+        
+        
+        protected override bool IsDontDestroyOnLoad => isDontDestroyOnLoad;
+        
+        protected override bool IsDestroyPrevInstance => isDestroyPrevInstance;
+        
+        private Type SingletonType => _singletonType ??= singletonType.GetType();
+        private Type _singletonType;
+        
+        
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            
+            EnsureInstance(this, instances.GetValueOrDefault(SingletonType), t => instances[SingletonType] = t);
+
+            if (instances.GetValueOrDefault(SingletonType) == this && IsDontDestroyOnLoad)
+            {
+                SceneManager.sceneLoaded += OnSceneLoaded;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            if (instances.GetValueOrDefault(SingletonType) == this)
+            {
+                SceneManager.sceneLoaded -= OnSceneLoaded;
+                instances.Remove(SingletonType);
+            }
+        }
+        
+        
+        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (DestroyNotMonoSingletonType) DestroyNotMonoSingletonOfType(SingletonType);
+        }
+
+        private void DestroyNotMonoSingletonOfType(Type type) => FindObjectsByType(type, FindObjectsInactive.Include, FindObjectsSortMode.InstanceID)
+            .Cast<Component>()
+            .Where(t => t.GetComponent<MonoSingleton>() ==null)
+            .ForEach(t => 
+            {
+                Debug.Log($"Destroy Not Mono Singleton Type: {t.transform.GetPath()}");
+                Destroy(t.gameObject);
+            });
+    }
+}
--- Runtime/Scripts/Managers/MonoSingleton.cs
+++ Runtime/Scripts/Managers/MonoSingleton.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using LCHFramework.Components;
-using LCHFramework.Extensions;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using Object = UnityEngine.Object;
 
 namespace LCHFramework.Managers
@@ -30,75 +26,6 @@ namespace LCHFramework.Managers
         protected virtual Object DestroyTarget => gameObject;
     }
     
-    [DefaultExecutionOrder(EventSystemExecutionOrder - 1)]
-    public class MonoSingleton : AbstractMonoSingleton
-    {
-        private const int EventSystemExecutionOrder = -1000;
-        
-        
-        
-        private static readonly Dictionary<Type, MonoSingleton> instances = new();
-        
-        
-        
-        [SerializeField] private Component singletonType;
-        [SerializeField] private bool destroyNotMonoSingletonTypeOnSceneLoaded = true;
-        [SerializeField] private bool isDontDestroyOnLoad;
-        [SerializeField] private bool isDestroyPrevInstance = true;
-        
-        
-        
-        protected override bool IsDontDestroyOnLoad => isDontDestroyOnLoad;
-
-        protected override bool IsDestroyPrevInstance => isDestroyPrevInstance;
-        
-        private Type SingletonType => _singletonType ??= singletonType.GetType();
-        private Type _singletonType;
-
-
-        
-#if UNITY_EDITOR
-        private void OnValidate()
-        {
-            destroyNotMonoSingletonTypeOnSceneLoaded = true;
-        }
-#endif
-
-        protected override void Awake()
-        {
-            base.Awake();
-            
-            SceneManager.sceneLoaded += OnSceneLoaded;
-            
-            EnsureInstance(this, instances.GetValueOrDefault(SingletonType), t => instances[SingletonType] = t);
-            
-            if (instances.GetValueOrDefault(SingletonType) == singletonType && IsDontDestroyOnLoad) DontDestroyOnLoad(gameObject);
-        }
-
-        protected virtual void OnDestroy()
-        {
-            SceneManager.sceneLoaded -= OnSceneLoaded;
-            
-            if (instances.GetValueOrDefault(SingletonType) == singletonType) instances.Remove(SingletonType);
-        }
-        
-        
-        
-        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-        {
-            if (destroyNotMonoSingletonTypeOnSceneLoaded) DestroyNotMonoSingletonOfType(SingletonType);
-        }
-
-        private void DestroyNotMonoSingletonOfType(Type type) => FindObjectsByType(type, FindObjectsInactive.Include, FindObjectsSortMode.InstanceID)
-            .Cast<Component>()
-            .Where(t => t.GetComponent<MonoSingleton>() ==null)
-            .ForEach(t => 
-            {
-                Debug.Log($"Destroy: {t.transform.GetPath()}");
-                Destroy(t.gameObject);
-            });
-    }
-    
     public class MonoSingleton<T> : AbstractMonoSingleton where T : MonoSingleton<T>
     {
         public static bool InstanceIsNull => Instance is null;
@@ -121,13 +48,19 @@ namespace LCHFramework.Managers
             base.Awake();
             
             Instance = (object)this as T;
-            
-            if (Instance == this && IsDontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+
+            if (Instance == this && IsDontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
         }
         
         protected virtual void OnDestroy()
         {
-            if (Instance == this) Instance = null;
+            if (Instance == this)
+            {
+                Instance = null;
+            }
         }
     }
 }
