f902b1e25890bf4d7a1a74a28e7a1209314b7bde
Lee Changhyun <ckdgus4002@gmail.com>
2025-07-29 23:19:14 +0900

    Auto Open 기능은 Editor 가 처음 Open 된 시점을 Invoke 할 수 없어 삭제.

--- Editor/Scripts/DefaultSceneOpener.cs
+++ Editor/Scripts/DefaultSceneOpener.cs
@@ -1,10 +1,6 @@
 using System.Linq;
-using LCHFramework.Extensions;
 using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEditorInternal;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace LCHFramework.Editor
 {
@@ -12,23 +8,9 @@ namespace LCHFramework.Editor
     {
         private static EditorBuildSettingsScene DefaultSceneOrNull => _defaultSceneOrNull ??= EditorBuildSettings.scenes.FirstOrDefault();
         private static EditorBuildSettingsScene _defaultSceneOrNull;
-    
-        [InitializeOnLoadMethod]
-        private static void InitializeOnLoadMethod()
-        {
-            var noPreviousScenes = SceneManager.GetActiveScene().name == "Untitled" && EditorSceneManager.GetSceneManagerSetup().IsEmpty();
-            if (noPreviousScenes) return;
-            
-            if (DefaultSceneOrNull == null) return;
-            
-            EditorApplication.delayCall += () =>
-            {
-                Debug.Log($"Default Scene Opened: {DefaultSceneOrNull.path}");
-                OpenDefaultScene();
-                InternalEditorUtility.RepaintAllViews();
-            };
-        }
-
+        
+        
+        
         [MenuItem(LCHFramework.MenuItemRootPath + "/" + "Open Default Scene")]
         private static void OnMenuItemClick()
         {
@@ -36,7 +18,7 @@ namespace LCHFramework.Editor
             
             OpenDefaultScene();
         }
-
+        
         private static void OpenDefaultScene()
         {
             EditorSceneManager.OpenScene(DefaultSceneOrNull.path);
