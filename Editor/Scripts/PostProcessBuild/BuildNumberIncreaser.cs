f5e6c2f82e39ee5590b181d5caf1ab1ef4341e3e
Lee Changhyun <ckdgus4002@gmail.com>
2025-09-07 01:37:29 +0900

    Commit.

--- Editor/Scripts/PostProcessBuild/BuildNumberIncreaser.cs
+++ Editor/Scripts/PostProcessBuild/BuildNumberIncreaser.cs
@@ -7,20 +7,18 @@ namespace LCHFramework.Editor
     public static class BuildNumberIncreaser
     {
         private const string MenuItemRootPath = LCHFramework.MenuItemRootPath + "/" + "Build Number Increaser";
-        private const string EnableMenuItemPath = MenuItemRootPath/* + "/" + nameof(Enabled)*/;
+        private const string EnabledMenuItemPath = MenuItemRootPath/* + "/" + nameof(Enabled)*/;
         
         private static readonly string EnabledPrefsKey = $"{nameof(BuildNumberIncreaser)}{nameof(Enabled)}";
         
         
-        private static bool Enabled
-        {
-            get => EditorPrefs.GetBool(EnabledPrefsKey, true);
-            set => EditorPrefs.SetBool(EnabledPrefsKey, value);
-        }
+        private static bool Enabled { get => EditorPrefs.GetBool(EnabledPrefsKey, true); set => EditorPrefs.SetBool(EnabledPrefsKey, value); }
         
         
         
-        [MenuItem(EnableMenuItemPath)] private static void EnableMenuItem() => Menu.SetChecked(EnableMenuItemPath, Enabled = !Enabled);
+        [MenuItem(EnabledMenuItemPath, true)] private static bool ValidateEnabledMenuItem() { Menu.SetChecked(EnabledMenuItemPath, Enabled); return true; }
+        
+        [MenuItem(EnabledMenuItemPath)] private static void EnableMenuItem() { Enabled = !Enabled; EditorApplication.RepaintHierarchyWindow(); }
         
         [PostProcessBuild]
         private static void Increase(BuildTarget buildTarget, string pathToBuiltProject)
