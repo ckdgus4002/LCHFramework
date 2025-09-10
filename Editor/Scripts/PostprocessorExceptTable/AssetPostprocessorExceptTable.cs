7c8bf9a240181ebc3ca282228d27723bd2812dcb
Lee Changhyun <ckdgus4002@gmail.com>
2025-09-11 00:15:40 +0900

    - LoadAssetByType() 추가 - LoadAssetsByType() 으로 이름 변경.

--- Editor/Scripts/PostprocessorExceptTable/AssetPostprocessorExceptTable.cs
+++ Editor/Scripts/PostprocessorExceptTable/AssetPostprocessorExceptTable.cs
@@ -14,7 +14,7 @@ namespace LCHFramework.Editor
             {
                 if (_instances == null || _instancesTime != Time.frameCount)
                 {
-                    _instances = AssetDatabaseUtility.LoadAssetAtTypes<T>($"{typeof(T).Name}");
+                    _instances = AssetDatabaseUtility.LoadAssetsByType<T>($"{typeof(T).Name}");
                     _instancesTime = Time.frameCount;
                 }
 
--- Editor/Scripts/Utilities/AssetDatabaseUtility.cs
+++ Editor/Scripts/Utilities/AssetDatabaseUtility.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -6,10 +7,14 @@ namespace LCHFramework.Editor.Utilities
 {
     public static class AssetDatabaseUtility
     {
-        public static T[] LoadAssetAtTypes<T>(string filter, string[] searchInFolders = null) where T : Object
+        public static T[] LoadAssetsByType<T>(string filter, string[] searchInFolders = null) where T : Object
+            => _LoadAssetsByType<T>(filter, searchInFolders).Where(t => t != null).ToArray();
+
+        public static T LoadAssetByType<T>(string filter, string[] searchInFolders = null) where T : Object
+            => _LoadAssetsByType<T>(filter, searchInFolders).FirstOrDefault(t => t != null);
+
+        private static IEnumerable<T> _LoadAssetsByType<T>(string filter, string[] searchInFolders = null) where T : Object
             => AssetDatabase.FindAssets(filter, searchInFolders)
-                .Select(guid => AssetDatabase.LoadAssetByGUID<T>(new GUID(guid)))
-                .Where(t => t != null)
-                .ToArray();
+                .Select(guid => AssetDatabase.LoadAssetByGUID<T>(new GUID(guid)));
     }
 }
