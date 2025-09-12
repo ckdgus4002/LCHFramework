12f8ae0c99e26daaba5b5681582dde5b73c13838
Lee Changhyun <ckdgus4002@gmail.com>
2025-09-12 21:08:45 +0900

    UniRx 로 대체.

--- Runtime/Scripts/Managers/LCHFramework.cs
+++ Runtime/Scripts/Managers/LCHFramework.cs
@@ -20,47 +20,6 @@ namespace LCHFramework
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void RuntimeInitializeOnLoadMethod() => CreateGameObjectIfInstanceIsNull();
         
-        public static Coroutine Delay(float seconds, Action callback) => Delay(Instance, seconds, callback);
-        
-        public static Coroutine Delay(MonoBehaviour monoBehaviour, float seconds, Action callback)
-        {
-            return monoBehaviour.StartCoroutine(Coroutine());
-            IEnumerator Coroutine()
-            {
-                var now = DateTime.Now;
-                yield return new WaitWhile(() => DateTime.Now < now.AddSeconds(seconds));
-                
-                callback?.Invoke();
-            }
-        }
-
-        public static Coroutine DelayEndOfFrame(Action callback) => DelayEndOfFrame(Instance, callback);
-        
-        public static Coroutine DelayEndOfFrame(MonoBehaviour monoBehaviour, Action callback)
-        {
-            return monoBehaviour.StartCoroutine(Coroutine());
-            IEnumerator Coroutine()
-            {
-                yield return new WaitForEndOfFrame();
-                
-                callback?.Invoke();
-            }
-        }
-        
-        public static Coroutine DelayFrame(int frameCount, Action callback) => DelayFrame(Instance, frameCount, callback);
-        
-        public static Coroutine DelayFrame(MonoBehaviour monoBehaviour, int frameCount, Action callback)
-        {
-            return monoBehaviour.StartCoroutine(Coroutine());
-            IEnumerator Coroutine()
-            {
-                var start = Time.frameCount;
-                yield return new WaitWhile(() => Time.frameCount < start + frameCount);
-                
-                callback?.Invoke();
-            }
-        }
-
         public static Coroutine PlayAnimations<T>(IEnumerable<(float, T)> animations, Action<int, T> action, bool loop = false) => PlayAnimations(Instance, animations, action, loop);
         
         public static Coroutine PlayAnimations<T>(MonoBehaviour monoBehaviour, IEnumerable<(float, T)> animations, Action<int, T> action, bool loop = false)
