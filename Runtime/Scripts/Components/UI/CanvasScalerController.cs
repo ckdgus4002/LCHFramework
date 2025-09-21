593897e90dd0f7ea730aa70e4319f85fd2296bb7
Lee Changhyun <ckdgus4002@gmail.com>
2025-09-21 20:28:32 +0900

    Commit.

--- Runtime/Scripts/Components/UI/CanvasScalerController.cs
+++ Runtime/Scripts/Components/UI/CanvasScalerController.cs
@@ -14,10 +14,10 @@ namespace LCHFramework.Components.UI
         
         private void Update()
         {
-            CanvasScaler.referenceResolution = LCHFramework.Instance.targetScreenResolution;
-            CanvasScaler.matchWidthOrHeight = Screen.width < Screen.height ? 0 : Screen.width == Screen.height ? .5f : 1;   
+            CanvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            var targetScreenResolution = LCHFramework.Instance.targetScreenResolution;
+            CanvasScaler.referenceResolution = targetScreenResolution;
+            CanvasScaler.matchWidthOrHeight = targetScreenResolution.x / targetScreenResolution.y < 1 ? 0 : Screen.width == Screen.height ? .5f : 1;   
         }
     }
 }
-
-
