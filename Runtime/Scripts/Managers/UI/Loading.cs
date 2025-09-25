704ab6d811493a20c846b558b75cca75abe7a09d
Lee Changhyun <ckdgus4002@gmail.com>
2025-09-25 22:42:01 +0900

    Commit.

--- Runtime/Scripts/Managers/UI/Loading.cs
+++ Runtime/Scripts/Managers/UI/Loading.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 
 namespace LCHFramework.Managers.UI
 {
-    public class Loading : MonoSingleton<Loading>
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Loading : MonoSingleton<Loading>, ILoadSceneUI
     {
         public const float DefaultFadeInTime = 0.5f;
         public const float DefaultFadeOutTime = 0.5f;
@@ -19,17 +20,17 @@ namespace LCHFramework.Managers.UI
         public Slider slider;
         
         
-        protected override bool IsDontDestroyOnLoad => transform.parent == null;
-        
         public override bool IsShown => Wrapper.activeSelf;
+        
+        protected override bool IsDontDestroyOnLoad => transform.parent == null;
         
         public virtual string[] LoadingMessages => _loadingMessages ??= new[] { DefaultLoadingMessage };
         private string[] _loadingMessages;
         
-        private CanvasGroup CanvasGroup => _canvasGroup == null ? _canvasGroup = GetComponentInChildren<CanvasGroup>() : _canvasGroup;
+        private CanvasGroup CanvasGroup => _canvasGroup == null ? _canvasGroup = GetComponent<CanvasGroup>() : _canvasGroup;
         private CanvasGroup _canvasGroup;
         
-        private GameObject Wrapper => _wrapper == null ? _wrapper = transform.Find(nameof(Wrapper)).gameObject : _wrapper;
+        private GameObject Wrapper => _wrapper == null ? _wrapper = transform.GetChild(0).gameObject : _wrapper;
         private GameObject _wrapper;
         
         
@@ -40,17 +41,20 @@ namespace LCHFramework.Managers.UI
             messageText = GetComponentsInChildren<TMP_Text>().FirstOrDefault(t => t.name.Contains("Message", StringComparison.OrdinalIgnoreCase) && t.name.Contains("Text", StringComparison.OrdinalIgnoreCase));
             slider = GetComponentInChildren<Slider>();
         }
-#endif
+#endif  
         
         
+        public Awaitable OnLoadSceneAsync(Func<string> getMessage, float fadeInDuration, float fadeOutDuration, Func<float> getPercentOrNull, Func<bool> getIsDone)
+            => LoadAsync(getMessage, fadeInDuration, fadeOutDuration, getPercentOrNull, getIsDone);
         
         public async Awaitable LoadAsync(Func<float> getPercentOrNull, Func<bool> getIsDone)
             => await LoadAsync(() => LoadingMessages.Pick(), DefaultFadeInTime, DefaultFadeOutTime, getPercentOrNull, getIsDone);
         
         public async Awaitable LoadAsync(Func<string> getMessage, float fadeInTime, float fadeOutTime, Func<float> getPercentOrNull, Func<bool> getIsDone)
         {
-            Wrapper.SetActive(true);
+            if (IsShown) return;
             
+            Wrapper.SetActive(true);
             var startTime = Time.time;
             while (true)
             {
@@ -73,9 +77,7 @@ namespace LCHFramework.Managers.UI
                 else break;
             }
             
-            Hide();
+            Wrapper.SetActive(false);
         }
-        
-        private void Hide() => Wrapper.SetActive(false);
     }
 }
