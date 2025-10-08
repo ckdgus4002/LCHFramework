eedf5478f84b7489b0f53f6dd6f28797a53ead13
Lee Changhyun <ckdgus4002@gmail.com>
2025-10-08 16:57:26 +0900

    Commit.

--- Runtime/Scripts/Components/UI/SafeArea.cs
+++ Runtime/Scripts/Components/UI/SafeArea.cs
@@ -41,11 +41,13 @@ namespace LCHFramework.Components.UI
             RectTransform.rotation = Quaternion.identity;
             RectTransform.localScale = Vector3.one;
             var scaleFactor = RootCanvasOrNull.renderMode != RenderMode.WorldSpace ? RootCanvasOrNull.scaleFactor : Screen.height / (RootCanvasOrNull.worldCamera.orthographicSize * 2);
+            if (scaleFactor == 0) return;
             var insetAverage = new Vector2(Screen.width - Screen.safeArea.width, Screen.height - Screen.safeArea.height) * 0.5f;
-            RectTransform.position = (Screen.safeArea.position - insetAverage) / scaleFactor;
+            var position = (Screen.safeArea.position - insetAverage) / scaleFactor;
+            RectTransform.localPosition = new Vector2(float.IsInfinity(position.x) ? 0 : position.x, float.IsInfinity(position.y) ? 0 : position.y);
             var size = Screen.safeArea.size / scaleFactor;
-            RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size.x);
-            RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size.y);
+            RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, float.IsInfinity(size.x) ? RectTransform.rect.width : size.x);
+            RectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, float.IsInfinity(size.y) ? RectTransform.rect.height : size.y);
             
             if (GetComponent<UIBehaviour>() != null) LayoutRebuilder.MarkLayoutForRebuild(RectTransform);
         }
