13fdbd35314a6da63637e129023c9646a9a697c5
Lee Changhyun <ckdgus4002@gmail.com>
2025-10-23 23:13:21 +0900

    CanvasUtility 추가.

---
+++ Runtime/Scripts/Utilities/CanvasUtility.cs
@@ -0,0 +1,18 @@
+using LCHFramework.Data;
+using LCHFramework.Managers;
+using UnityEngine;
+
+namespace LCHFramework.Utilities
+{
+    public static class CanvasUtility
+    {
+        public static float GetScaleFactor(Canvas canvas) => canvas.scaleFactor;
+        
+        public static float GetScaleFactor()  => OrientationManager.Instance.Orientation.Value switch
+        {
+            >= Orientation.Portrait and <= Orientation.PortraitUpsideDown => Screen.width / LCHFramework.Instance.targetScreenResolution.x,
+            >= Orientation.LandscapeLeft and <= Orientation.LandscapeRight => Screen.height / LCHFramework.Instance.targetScreenResolution.y,
+            _ => -1
+        };
+    }
+}
