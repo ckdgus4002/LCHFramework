3d6db34b5d5dbf0a45c8f8d984e8e2357cdf42ed
Lee Changhyun <ckdgus4002@gmail.com>
2025-11-12 00:56:19 +0900

    ListExtension/TryAdd() 추가.

--- Runtime/Scripts/Extensions/ListExtension.cs
+++ Runtime/Scripts/Extensions/ListExtension.cs
@@ -4,6 +4,13 @@ namespace LCHFramework.Extensions
 {
     public static class ListExtension
     {
+        public static bool TryAdd<T>(this List<T> list, T item)
+        {
+            var result = list.Contains(item); 
+            if (!result) list.Add(item);
+            return !result;
+        }
+                
         public static T AddAndReturnItem<T>(this List<T> list, T item)
         {
             list.Add(item);
