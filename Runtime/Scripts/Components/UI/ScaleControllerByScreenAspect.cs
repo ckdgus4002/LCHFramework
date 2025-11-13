ef6eb8e5704a3a1bad8bc045d9aefbf55eb59b82
Lee Changhyun <ckdgus4002@gmail.com>
2025-11-14 01:50:53 +0900

    minScale, maxScale 을 Vector3 로 변경.

--- Runtime/Scripts/Components/UI/ScaleControllerByScreenAspect.cs
+++ Runtime/Scripts/Components/UI/ScaleControllerByScreenAspect.cs
@@ -10,31 +10,31 @@ namespace LCHFramework.Components.UI
     {
         public float aspectRatio = 1;
         public bool useMinScale;
-        [ShowInInspector(nameof(useMinScale))] public float minScale = 1;
+        [ShowInInspector(nameof(useMinScale))] public Vector3 minScale = Vector3.one;
         public bool useMaxScale;
-        [ShowInInspector(nameof(useMaxScale))] public float maxScale = 1;
+        [ShowInInspector(nameof(useMaxScale))] public Vector3 maxScale = Vector3.one;
         
         
-        [NonSerialized] private float _prevScreenAspect;
-        [NonSerialized] private float _prevMinScale;
-        [NonSerialized] private float _prevMaxScale;
+        [NonSerialized] private float _prevScreenAspectRatio;
+        [NonSerialized] private Vector3 _prevMinScale;
+        [NonSerialized] private Vector3 _prevMaxScale;
         
         
         
         protected override void OnReset()
         {
-            _prevScreenAspect = float.MinValue;
-            _prevMinScale = float.MinValue;
-            _prevMaxScale = float.MinValue;
+            _prevScreenAspectRatio = float.MinValue;
+            _prevMinScale = Vector3Utility.New(float.MinValue);
+            _prevMaxScale = Vector3Utility.New(float.MinValue);
         }
         
         
         
         protected override bool ScaleIsChanged()
         {
-            var screenAspect = (float)Screen.width / Screen.height;
-            var result = !Mathf.Approximately(_prevScreenAspect, screenAspect) || (useMinScale && !Mathf.Approximately(_prevMinScale, minScale)) || (useMaxScale && !Mathf.Approximately(_prevMaxScale, maxScale));
-            _prevScreenAspect = screenAspect;
+            var screenAspectRatio = Screen.AspectRatio;
+            var result = !Mathf.Approximately(_prevScreenAspectRatio, screenAspectRatio) || (useMinScale && _prevMinScale != minScale) || (useMaxScale && _prevMaxScale != maxScale);
+            _prevScreenAspectRatio = screenAspectRatio;
             _prevMinScale = minScale;
             _prevMaxScale = maxScale;
             return result;
@@ -45,12 +45,14 @@ namespace LCHFramework.Components.UI
             Tracker.Clear();
             Tracker.Add(this, RectTransform, DrivenTransformProperties.Scale);
             
-            var screenAspect = (float)Screen.width / Screen.height;
-            var scale = screenAspect / aspectRatio;
-            RectTransform.localScale = Vector3Utility.New(useMinScale && useMaxScale ? Mathf.Clamp(scale, minScale, maxScale)
-                    : useMinScale && !useMaxScale ? Mathf.Max(scale, minScale)
-                    : !useMinScale && useMaxScale ? Mathf.Min(scale, maxScale)
-                    : scale);
+            var scale = Screen.AspectRatio / aspectRatio;
+            RectTransform.localScale = useMinScale switch
+            {
+                false when !useMaxScale => new Vector3(scale, scale, scale),
+                false when useMaxScale => new Vector3(Mathf.Min(scale, maxScale.x), Mathf.Min(scale, maxScale.y), Mathf.Min(scale, maxScale.z)),
+                true when !useMaxScale => new Vector3(Mathf.Max(scale, minScale.x), Mathf.Max(scale, minScale.y), Mathf.Max(scale, minScale.z)),
+                _ => new Vector3(Mathf.Clamp(scale, minScale.x, maxScale.x), Mathf.Clamp(scale, minScale.y, maxScale.y), Mathf.Clamp(scale, minScale.z, maxScale.z))
+            };
             
             LayoutRebuilder.ForceRebuildLayoutImmediate(RectTransform);
         }
