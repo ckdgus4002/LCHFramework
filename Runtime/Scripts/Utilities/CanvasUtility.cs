390678381e5a1c3d2b7ee41935ab921a58413ada
Lee Changhyun <ckdgus4002@gmail.com>
2025-11-30 15:01:28 +0900

    GetScaleFactor() 정리.

--- Runtime/Scripts/Utilities/CanvasUtility.cs
+++ Runtime/Scripts/Utilities/CanvasUtility.cs
@@ -6,17 +6,20 @@ namespace LCHFramework.Utilities
 {
     public static class CanvasUtility
     {
-        public static float GetScaleFactor(Canvas canvas) => canvas.scaleFactor;
+        public static float GetScaleFactor(Canvas canvas) => canvas.renderMode != RenderMode.WorldSpace ? canvas.scaleFactor : GetScaleFactor(LCHFramework.Instance.targetScreenResolution);
         
-        public static float GetScaleFactor() => GetScaleFactor(OrientationManager.Instance.Orientation.Value, LCHFramework.Instance.targetScreenResolution);
-        
-        public static float GetScaleFactor(Vector2 targetScreenResolution) => GetScaleFactor(targetScreenResolution.x <= targetScreenResolution.y ? Orientation.Portrait : Orientation.LandscapeLeft, targetScreenResolution);
-        
-        public static float GetScaleFactor(Orientation orientation, Vector2 targetScreenResolution) => orientation switch
+        public static float GetScaleFactor(Vector2 screenSize)
         {
-            >= Orientation.Portrait and <= Orientation.PortraitUpsideDown => Screen.width / targetScreenResolution.x,
-            >= Orientation.LandscapeLeft and <= Orientation.LandscapeRight => Screen.height / targetScreenResolution.y,
-            _ => -1
-        };
+            var orientation 
+                = OrientationManager.InstanceIsNull && screenSize.x <= screenSize.y ? Orientation.Portrait
+                : OrientationManager.InstanceIsNull && screenSize.y < screenSize.x ? Orientation.LandscapeLeft
+                : OrientationManager.Instance.Orientation.Value;
+            return orientation switch
+            {
+                >= Orientation.Portrait and <= Orientation.PortraitUpsideDown => Screen.width / screenSize.x,
+                >= Orientation.LandscapeLeft and <= Orientation.LandscapeRight => Screen.height / screenSize.y,
+                _ => -1
+            };
+        }
     }
 }
