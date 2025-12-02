8648e45e01d7f03490852b2f2e1b86cbf7ea8806
Lee Changhyun <ckdgus4002@gmail.com>
2025-12-03 08:04:26 +0900

    Commit.

--- Runtime/Scripts/Utilities/AwaitableUtility.cs
+++ Runtime/Scripts/Utilities/AwaitableUtility.cs
@@ -5,7 +5,7 @@ namespace LCHFramework.Utilities
 {
     public static class AwaitableUtility
     {
-        public static async Awaitable WaitUntil(Func<bool> predicate) => await WaitWhile(() => !predicate.Invoke());
+        public static async Awaitable WaitUntil(Func<bool> predicate) { while (!predicate.Invoke()) await Awaitable.NextFrameAsync(); }
         
         public static async Awaitable WaitWhile(Func<bool> predicate) { while (predicate.Invoke()) await Awaitable.NextFrameAsync(); }
 
