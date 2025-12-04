11e3aaebdbf3ac124aadaab97121beec7698eba3
Lee Changhyun <ckdgus4002@gmail.com>
2025-12-04 15:42:15 +0900

    UnityEventTExtension 추가 및 UnityEventExtension 정리.

--- Runtime/Scripts/Extensions/UnityEventTExtension.cs
+++ Runtime/Scripts/Extensions/UnityEventTExtension.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace LCHFramework.Extensions
 {
-    public static class UnityEventExtension
+    public static class UnityEventTExtension
     {
-        public static bool Contains(this UnityEvent unityEvent, Object target, string methodName)
+        public static bool Contains<T>(this UnityEvent<T> unityEvent, Object target, string methodName)
         {
             for (var i = 0; i < unityEvent.GetPersistentEventCount(); i++)
                 if (Equals(unityEvent, i, target, methodName))
@@ -14,7 +14,7 @@ namespace LCHFramework.Extensions
             return false;
         }
         
-        public static bool Equals(this UnityEvent unityEvent, int persistentIndex, Object target, string methodName)
+        public static bool Equals<T>(this UnityEvent<T> unityEvent, int persistentIndex, Object target, string methodName)
             => target == unityEvent.GetPersistentTarget(persistentIndex) && methodName == unityEvent.GetPersistentMethodName(persistentIndex);
     }
 }
