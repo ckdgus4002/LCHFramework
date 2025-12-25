7fc7101e2f632e7c6242a63d16883397bae01be7
Lee Changhyun <ckdgus4002@gmail.com>
2025-12-25 21:42:38 +0900

    Commit.

--- Runtime/Scripts/Utilities/AwaitableUtility.cs
+++ Runtime/Scripts/Utilities/AwaitableUtility.cs
@@ -6,11 +6,13 @@ namespace LCHFramework.Utilities
 {
     public static class AwaitableUtility
     {
+        public static Awaitable CompletedTask => FromResult();
+        
+        
+        
         public static async Awaitable WaitUntil(Func<bool> predicate, CancellationToken cancellationToken = default) { while (!predicate.Invoke()) await Awaitable.NextFrameAsync(cancellationToken); }
         
         public static async Awaitable WaitWhile(Func<bool> predicate, CancellationToken cancellationToken = default) { while (predicate.Invoke()) await Awaitable.NextFrameAsync(cancellationToken); }
-        
-        public static Awaitable CompletedTask => FromResult();
         
         public static Awaitable FromResult() => new AwaitableCompletionSource().Awaitable;
         
