788f4fe2600af5058ee43887ea585772432dda16
Lee Changhyun <ckdgus4002@gmail.com>
2025-12-31 01:50:11 +0900

    override Index.

--- Runtime/Scripts/Managers/StepManager/Step.cs
+++ Runtime/Scripts/Managers/StepManager/Step.cs
@@ -4,6 +4,10 @@ namespace LCHFramework.Managers.StepManager
 {
     public class Step : LCHMonoBehaviour
     {
+        public override int Index => StepManager.Instance.Steps.IndexOf(this);  
+        
+        
+        
         public virtual void Show() => gameObject.SetActive(true);
 
         public virtual void Hide() => gameObject.SetActive(false);
