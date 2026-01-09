15892c9df4977cabeb7111c25b6a591645b3690d
Lee Changhyun <ckdgus4002@gmail.com>
2026-01-10 00:15:27 +0900

    onShow, onHide 추가.

--- Runtime/Scripts/Managers/StepManager/Step.cs
+++ Runtime/Scripts/Managers/StepManager/Step.cs
@@ -1,15 +1,28 @@
+using System;
 using LCHFramework.Components;
 
 namespace LCHFramework.Managers.StepManager
 {
     public class Step : LCHMonoBehaviour
     {
+        public event Action onShow;
+        public event Action onHide;
+        
+        
         public override int Index => StepManager.Instance.Steps.IndexOf(this);  
         
         
         
-        public virtual void Show() => gameObject.SetActive(true);
+        public virtual void Show()
+        {
+            gameObject.SetActive(true);
+            onShow?.Invoke();
+        }
 
-        public virtual void Hide() => gameObject.SetActive(false);
+        public virtual void Hide()
+        {
+            onHide?.Invoke();
+            gameObject.SetActive(false);
+        }
     }
 }
