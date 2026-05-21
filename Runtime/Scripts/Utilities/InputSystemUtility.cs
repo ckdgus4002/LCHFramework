9b0327df4a2697ad7282f8a645d780a4f4822308
Lee Changhyun <ckdgus4002@gmail.com>
2026-05-21 22:47:54 +0900

    Commit.

--- Runtime/Scripts/Utilities/InputSystemUtility.cs
+++ Runtime/Scripts/Utilities/InputSystemUtility.cs
@@ -4,22 +4,28 @@ namespace LCHFramework.Utilities
 {
     public static class InputSystemUtility
     {
-        public static bool TryGetWasPressedThisFrame(out Pointer pointerOrNull)
-            => (pointerOrNull = Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasPressedThisFrame ? Touchscreen.current 
+        public static bool TryGetWasPressedThisFramePointerOrNull(out Pointer result) => (result = GetWasPressedThisFramePointerOrNull()) != null;
+        
+        public static bool TryGetIsPressedPointerOrNull(out Pointer result) => (result = GetIsPressedPointerOrNull()) != null;
+        
+        public static bool TryGetWasReleasedThisFramePointerOrNull(out Pointer result) => (result = GetWasReleasedThisFramePointerOrNull()) != null;
+        
+        public static Pointer GetWasPressedThisFramePointerOrNull()
+            => Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasPressedThisFrame ? Touchscreen.current 
                 : Pen.current != null && Pen.current.tip.wasPressedThisFrame ? Pen.current
                 : Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame ? Mouse.current
-                : null) != null;
+                : null;
         
-        public static bool TryGetIsPressed(out Pointer pointerOrNull)
-            => (pointerOrNull = Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed ? Touchscreen.current 
+        public static Pointer GetIsPressedPointerOrNull()
+            => Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed ? Touchscreen.current 
                 : Pen.current != null && Pen.current.tip.isPressed ? Pen.current
                 : Mouse.current != null && Mouse.current.leftButton.isPressed ? Mouse.current
-                : null) != null;
+                : null;
         
-        public static bool TryGetWasReleasedThisFrame(out Pointer pointerOrNull)
-            => (pointerOrNull = Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasReleasedThisFrame ? Touchscreen.current 
+        public static Pointer GetWasReleasedThisFramePointerOrNull()
+            => Touchscreen.current != null && Touchscreen.current.primaryTouch.press.wasReleasedThisFrame ? Touchscreen.current 
                 : Pen.current != null && Pen.current.tip.wasReleasedThisFrame ? Pen.current
                 : Mouse.current != null && Mouse.current.leftButton.wasReleasedThisFrame ? Mouse.current
-                : null) != null;
+                : null;
     }
 }
