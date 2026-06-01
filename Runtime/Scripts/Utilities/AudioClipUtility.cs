ae89f308eda5bd6d8494bda735c753fceffb1616
Lee Changhyun <ckdgus4002@gmail.com>
2026-06-01 23:59:44 +0900

    AudioClipUtility 추가.

--- Runtime/Scripts/Utilities/AudioClipUtility.cs
+++ Runtime/Scripts/Utilities/AudioClipUtility.cs
@@ -4,13 +4,7 @@ namespace LCHFramework.Utilities
 {
     public static class AudioClipUtility
     {
-        public static AudioClip Default
-        {
-            get
-            {
-                var outputSampleRate = AudioSettings.outputSampleRate;
-                return AudioClip.Create("NewClip", outputSampleRate, 1, outputSampleRate, false);
-            }
-        }
+        public static AudioClip Default => _default == null ? _default = AudioClip.Create("NewClip", 1, 1, AudioSettings.outputSampleRate, false) : _default;
+        private static AudioClip _default;
     }
 }
