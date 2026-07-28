efba1fb300f41e8f90eb4b3be6621c858cd3aa28
Lee Changhyun <ckdgus4002@gmail.com>
2026-07-29 05:39:56 +0900

    Commit.

--- Editor/Scripts/KeystoreSetter.cs
+++ Editor/Scripts/KeystoreSetter.cs
@@ -9,9 +9,6 @@ namespace LCHFramework.Editor
     {
         private const string MenuItemRootPath = LCHFramework.MenuItemRootPath + "/" + nameof(KeystoreSetter);
         private const string EnabledMenuItemPath = MenuItemRootPath + "/" + nameof(Enabled);
-        private const string KeystorePass = "KeystorePass: ";
-        private const string KeyaliasName = "KeyaliasName: ";
-        private const string KeyaliasPass = "KeyaliasPass: ";
         
         private static readonly string EnabledPrefsKey = $"{nameof(KeystoreSetter)}{nameof(Enabled)}";
         
@@ -32,25 +29,30 @@ namespace LCHFramework.Editor
         
         [InitializeOnLoadMethod] private static void InitializeOnLoadMethod() { if (Enabled) Set(); }
         
-        private static bool _isSet;
         private static void Set(bool force = false)
         {
-            if (!force && _isSet) return;
+            if (!force) return;
             
             if (string.IsNullOrWhiteSpace(PlayerSettings.Android.keystoreName)) return;
             
             var keystoreFileInfo = new FileInfo(PlayerSettings.Android.keystoreName);
             if (!keystoreFileInfo.Exists) return;
             
-            var keystorePasswordFileInfoOrNull = new FileInfo($"{keystoreFileInfo.Directory.FullName}{Path.DirectorySeparatorChar}KeystorePassword.txt");
-            if (!keystorePasswordFileInfoOrNull.Exists) return;
+            var keystorePasswordFileInfo = new FileInfo($"{keystoreFileInfo.Directory!.FullName}/KeystorePassword.txt");
+            if (!keystorePasswordFileInfo.Exists) return;
             
-            foreach (var line in File.ReadAllLines(keystorePasswordFileInfoOrNull.FullName))
+            Set(keystorePasswordFileInfo);
+        }
+        
+        public static void Set(FileInfo keystorePasswordFileInfo)
+        {
+            const string KeystorePass = "KeystorePass: ";
+            const string KeyaliasName = "KeyaliasName: ";
+            const string KeyaliasPass = "KeyaliasPass: ";
+            foreach (var line in File.ReadAllLines(keystorePasswordFileInfo.FullName))
                 if (line.Contains(KeystorePass)) PlayerSettings.Android.keystorePass = line.Replace(KeystorePass, "");
                 else if (line.Contains(KeyaliasName)) PlayerSettings.Android.keyaliasName = line.Replace(KeyaliasName, "");
                 else if (line.Contains(KeyaliasPass)) PlayerSettings.Android.keyaliasPass = line.Replace(KeyaliasPass, "");
-            
-            _isSet = true;
         }
     }
 }
