42e0501db9d34f3e3042d3996c4d8f02eb62e973
Lee Changhyun <ckdgus4002@gmail.com>
2026-08-07 12:23:51 +0900

    Commit.

--- Runtime/Scripts/Managers/OrientationManager.cs
+++ Runtime/Scripts/Managers/OrientationManager.cs
@@ -5,39 +5,40 @@ namespace LCHFramework.Managers
 {
     public class OrientationManager : OrientationManager<OrientationManager>
     {
-        [RuntimeInitializeOnLoadMethod]
-        private static void RuntimeInitializeOnLoadMethod() => CreateGameObjectIfInstanceIsNull();
     }
     
     [ExecuteAlways]
     public class OrientationManager<T> : MonoSingleton<T> where T : OrientationManager<T>
     {
+        public ReactiveProperty<ScreenOrientation> Orientation { get; } = new();
+        
+        
         protected override bool IsDontDestroyOnLoad => transform.parent == null;
         
         public override bool IsDestroyPrevInstance => false;
         
-        public ReactiveProperty<ScreenOrientation> Orientation { get; } = new();
+        protected virtual bool? IsPreferredLandscapeOrientation => LCHFramework.InstanceIsNull ? null : LCHFramework.Instance.isPreferredLandscapeOrientation;
         
         
         
         protected virtual void Update()
         {
+            var screenAspectRatio = Screen.AspectRatio;
             switch (UnityEngine.Application.isEditor)
             {
-                case true when UnityEngine.Application.isPlaying:
+                case true:
                 {
-                    Orientation.Value = Screen.AspectRatio < 1 || (Mathf.Approximately(Screen.AspectRatio, 1) && !LCHFramework.Instance.isPreferredLandscapeOrientation) ? ScreenOrientation.Portrait : ScreenOrientation.LandscapeLeft;
+                    var isScreenAspectRatioOne = Mathf.Approximately(screenAspectRatio, 1);
+                    var isPreferredLandscapeOrientation = IsPreferredLandscapeOrientation;
+                    Orientation.Value = screenAspectRatio < 1 || (isScreenAspectRatioOne && isPreferredLandscapeOrientation != null && !(bool)isPreferredLandscapeOrientation) ? ScreenOrientation.Portrait
+                        : isScreenAspectRatioOne && isPreferredLandscapeOrientation == null ? ScreenOrientation.Unknown
+                        : ScreenOrientation.LandscapeLeft;
                     break;
                 }
                 case false:
                 {
                     var orientationIndex = UnityEngine.Screen.orientation != UnityEngine.ScreenOrientation.AutoRotation ? (int)UnityEngine.Screen.orientation : (int)Input.deviceOrientation;
                     Orientation.Value = orientationIndex is < 1 or > 4 ? Orientation.Value : (ScreenOrientation)orientationIndex;
-                    break;
-                }
-                default:
-                {
-                    Orientation.Value = ScreenOrientation.Unknown;
                     break;
                 }
             }
