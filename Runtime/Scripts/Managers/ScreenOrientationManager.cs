43e6e7857118a48486377c63762e06c3df28f500
Lee Changhyun <ckdgus4002@gmail.com>
2026-08-07 13:23:08 +0900

    OrientationManager -> ScreenOrientationManager 이름 변경.

--- Runtime/Scripts/Managers/ScreenOrientationManager.cs
+++ Runtime/Scripts/Managers/ScreenOrientationManager.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 namespace LCHFramework.Managers
 {
-    public class OrientationManager : OrientationManager<OrientationManager>
+    public class ScreenOrientationManager : ScreenOrientationManager<ScreenOrientationManager>
     {
     }
     
     [ExecuteAlways]
-    public class OrientationManager<T> : MonoSingleton<T> where T : OrientationManager<T>
+    public class ScreenOrientationManager<T> : MonoSingleton<T> where T : ScreenOrientationManager<T>
     {
-        public ReactiveProperty<ScreenOrientation> Orientation { get; } = new();
+        public ReactiveProperty<ScreenOrientation> Value { get; } = new();
         
         
         protected override bool IsDontDestroyOnLoad => transform.parent == null;
@@ -18,8 +18,6 @@ namespace LCHFramework.Managers
         public override bool IsDestroyPrevInstance => false;
         
         protected virtual bool? IsPreferredLandscapeOrientation => LCHFramework.InstanceIsNull ? null : LCHFramework.Instance.isPreferredLandscapeOrientation;
-        
-        protected int OrientationIndex => (int)Orientation.Value;
         
         
         
@@ -32,15 +30,15 @@ namespace LCHFramework.Managers
                 {
                     var isScreenAspectRatioOne = Mathf.Approximately(screenAspectRatio, 1);
                     var isPreferredLandscapeOrientation = IsPreferredLandscapeOrientation;
-                    Orientation.Value = screenAspectRatio < 1 || (isScreenAspectRatioOne && isPreferredLandscapeOrientation != null && !(bool)isPreferredLandscapeOrientation) ? ScreenOrientation.Portrait
+                    Value.Value = screenAspectRatio < 1 || (isScreenAspectRatioOne && isPreferredLandscapeOrientation != null && !(bool)isPreferredLandscapeOrientation) ? ScreenOrientation.Portrait
                         : isScreenAspectRatioOne && isPreferredLandscapeOrientation == null ? ScreenOrientation.Unknown
                         : ScreenOrientation.LandscapeLeft;
                     break;
                 }
                 case false:
                 {
-                    var unityEnginOrientationIndex = UnityEngine.Screen.orientation != UnityEngine.ScreenOrientation.AutoRotation ? (int)UnityEngine.Screen.orientation : (int)Input.deviceOrientation;
-                    Orientation.Value = unityEnginOrientationIndex is < 1 or > 4 ? Orientation.Value : (ScreenOrientation)unityEnginOrientationIndex;
+                    var orientationIndex = UnityEngine.Screen.orientation != UnityEngine.ScreenOrientation.AutoRotation ? (int)UnityEngine.Screen.orientation : (int)Input.deviceOrientation;
+                    Value.Value = orientationIndex is < 1 or > 4 ? Value.Value : (ScreenOrientation)orientationIndex;
                     break;
                 }
             }
