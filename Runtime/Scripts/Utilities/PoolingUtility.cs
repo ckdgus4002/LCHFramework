09b754b5806b9cbede8d60b5a42b78fc935f24d4
Lee Changhyun <ckdgus4002@gmail.com>
2026-08-17 17:04:20 +0900

    Commit.

--- Runtime/Scripts/Utilities/PoolingUtility.cs
+++ Runtime/Scripts/Utilities/PoolingUtility.cs
@@ -5,7 +5,7 @@ namespace LCHFramework.Utilities
 {
     public static class PoolingUtility
     {
-        public static void Pooling<T>(int itemCount, ref List<T> list, Func<T> createFunc, Action<int, T> actionOnAddOrNull = null, Action<T> actionOnRemoveOrNull = null)
+        public static void Pooling<T>(int itemCount, ref List<T> list, Func<T> createFunc, Action<T, int> actionOnAddOrNull = null, Action<T> actionOnRemoveOrNull = null)
         {
             for (var i = 0; i < Math.Max(itemCount, list.Count);)
             {
@@ -13,7 +13,7 @@ namespace LCHFramework.Utilities
                 {
                     var item = list.Count <= i ? createFunc.Invoke() : list[i];
                     if (!list.Contains(item)) list.Add(item);
-                    actionOnAddOrNull?.Invoke(i, item);
+                    actionOnAddOrNull?.Invoke(item, i);
                     ++i;
                 }
                 else
